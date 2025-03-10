6b34c5ee6fe5e09919361aaf96299defa92586aa
Christian Fuentes <christian.fuenteszelada@gmail.com>
2025-03-09 23:22:33 -0300

    Creación de componentes del sidebar, header y options.

--- src/app/gifs/pages/dahsboard-page/dahsboard-page.component.ts
+++ src/app/gifs/pages/dahsboard-page/dahsboard-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenuHeaderComponent } from '../../components/side-menu-header/side-menu-header.component';
+import { SideMenuOptionsComponent } from '../../components/side-menu-options/side-menu-options.component';
 
 @Component({
   selector: 'app-dahsboard-page',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideMenuHeaderComponent, SideMenuOptionsComponent],
   templateUrl: './dahsboard-page.component.html',
   styleUrl: './dahsboard-page.component.css'
 })
