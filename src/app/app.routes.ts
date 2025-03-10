b48e60e61490cf1ef2e14476c59550bbf92ad74b
Christian Fuentes <christian.fuenteszelada@gmail.com>
2025-03-09 22:15:24 -0300

    Creación de rutas y componentes dashboard, trending y serach

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // este path esta cargado con lazy load (carga peresoza)
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./gifs/pages/dahsboard-page/dahsboard-page.component').then(m => m.DahsboardPageComponent),
+
+    children: [
+      {
+        path: 'trending',
+        loadComponent: () =>
+          import('./gifs/pages/trending-page/trending-page.component').then(m => m.TrendingPageComponent)
+      },
+
+      {
+        path: 'search',
+        loadComponent: () =>
+          import('./gifs/pages/search-page/search-page.component').then(m => m.SearchPageComponent)
+      },
+      {
+        path: '**',
+        redirectTo: 'trending',
+      }
+
+    ]
+  },
+
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  }
+
+];
---
+++ src/app/gifs/pages/dahsboard-page/dahsboard-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-dahsboard-page',
+  imports: [RouterOutlet],
+  templateUrl: './dahsboard-page.component.html',
+  styleUrl: './dahsboard-page.component.css'
+})
+export class DahsboardPageComponent {
+
+}
