d579847e7c1923e0c2e3d57ea78c541556dfdb38
Christian Fuentes <christian.fuenteszelada@gmail.com>
2025-03-16 23:57:40 -0300

    Creacionm de binding y el archivo de environments de desarrollo y producción.

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+
+  production: true,
+  companyName: 'Gifs',
+  conpanyName2: 'App',
+  companySlogan: 'maneja tus gifs',
+
+  // ApiKeys
+
+  giphyApiKey: 'arqC855dIidnPuja9lbUSWSnFiMk3P8E'
+
+}
